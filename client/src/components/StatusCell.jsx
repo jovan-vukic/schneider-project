aafce8bff728f1a944f73315d1bf700faa7f1318
jovan-vukic <jovanv0803@gmail.com>
2024-10-20 01:53:04 +0200

    Show initial values and color Status cells

--- client/src/components/StatusCell.jsx
+++ client/src/components/StatusCell.jsx
@@ -2,10 +2,20 @@ import { Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import React from "react";
 
 const StatusCell = ({ getValue, row, column, table }) => {
+  /* If status is null, default to an empty object */
+  const { name, color } = getValue() || {};
+
   return (
     <Menu isLazy offset={[0, 0]} flip={false} autoSelect={false}>
-      <MenuButton aria-label="Status" w="100%" h="100%" p={1.5}>
-        Status
+      <MenuButton
+        aria-label="Status"
+        w="100%"
+        h="100%"
+        p={1.5}
+        bg={color}
+        color="gray.800"
+      >
+        {name}
       </MenuButton>
       <MenuList>
         <MenuItem>New Tab</MenuItem>
